021c6fd6665f9f3011ad326feaa5e0946488b76f
Jack Sweeney <jackcsweeney@gmail.com>
2024-04-30 10:27:19 -0700

    feat: Add route for creating subscription via HTTP request

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v0 do 
+      resources :subscriptions, only: [:create]
+    end
+  end
 end
